9dbca3d983e2e19242b4e2903bf1a3492a337ac7
yelkov <yelkovq@gmail.com>
2025-01-03 12:41:12 +0100

    test(HuecoDAOTest.java): completar test HuecosDAO

--- src/test/java/edu/badpals/estudio/model/cabina/HuecoDAOTest.java
+++ src/test/java/edu/badpals/estudio/model/cabina/HuecoDAOTest.java
@@ -4,6 +4,7 @@ import edu.badpals.estudio.model.utils.EntityManagerFactoryProvider;
 import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,4 +38,66 @@ class HuecoDAOTest {
         assertEquals(hueco.getDia(), LocalDate.of(2024,12,16));
     }
 
+    @Test
+    @Order(2)
+    public void test_findAllFromCabina(){
+        Cabina cabina = cabinaService.getCabina(1);
+        List<Hueco> huecosCabina1 = huecoDAO.findAllFromCabina(cabina);
+
+        assertNotNull(huecosCabina1);
+        assertEquals(54, huecosCabina1.size());
+    }
+
+    @Test
+    @Order(3)
+    public void test_findHuecosLibres(){
+        List<Hueco> huecosLibresDia16 = huecoDAO.findHuecosLibres(LocalDate.of(2024,12,16),LocalDate.of(2024,12,16));
+        assertNotNull(huecosLibresDia16);
+        assertEquals(36, huecosLibresDia16.size());
+    }
+
+    @Test
+    @Order(4)
+    public void test_findHuecoLibresNull(){
+        List<Hueco> huecosLibresDia3 = huecoDAO.findHuecosLibres(LocalDate.of(2023,12,3),LocalDate.of(2023,12,3));
+        assertTrue(huecosLibresDia3.isEmpty());
+    }
+
+    @Test
+    @Order(5)
+    public void test_findHuecosLibresRango(){
+        List<Hueco> huecosLibresDia16_18 = huecoDAO.findHuecosLibres(LocalDate.of(2024,12,16),LocalDate.of(2024,12,18));
+        assertNotNull(huecosLibresDia16_18);
+        assertEquals(108, huecosLibresDia16_18.size());
+    }
+
+    @Test
+    @Order(6)
+    public void test_findHuecosLibresCabina(){
+        Cabina cabina = cabinaService.getCabina(1);
+        List<Hueco> huecosLibresCabinaDia16 = huecoDAO.findHuecosLibresCabina(cabina,LocalDate.of(2024,12,16),LocalDate.of(2024,12,16));
+
+        assertNotNull(huecosLibresCabinaDia16);
+        assertEquals(9, huecosLibresCabinaDia16.size());
+    }
+
+    @Test
+    @Order(7)
+    public void test_findHuecosLibresCabinaNull(){
+        Cabina cabina = cabinaService.getCabina(1);
+        List<Hueco> huecosLibresCabinaDia3 = huecoDAO.findHuecosLibresCabina(cabina,LocalDate.of(2024,12,3),LocalDate.of(2024,12,3));
+
+        assertTrue(huecosLibresCabinaDia3.isEmpty());
+
+    }
+
+    @Test
+    @Order(8)
+    public void test_findHuecosLibresCabinaRango(){
+        Cabina cabina = cabinaService.getCabina(1);
+        List<Hueco> huecosLibresCabinaDia16 = huecoDAO.findHuecosLibresCabina(cabina,LocalDate.of(2024,12,16),LocalDate.of(2024,12,18));
+
+        assertNotNull(huecosLibresCabinaDia16);
+        assertEquals(27, huecosLibresCabinaDia16.size());
+    }
 }
