1fbe940d354124cd56911a49447b35dea8707ac1
Evan <evanchapela@gmail.com>
2025-01-05 17:32:39 +0100

    build(Cabina):finish Cabina

--- src/main/java/edu/badpals/estudio/controller/Controller.java
+++ src/main/java/edu/badpals/estudio/controller/Controller.java
@@ -1,6 +1,7 @@
 package edu.badpals.estudio.controller;
 import edu.badpals.estudio.model.cabina.Cabina;
 import edu.badpals.estudio.model.cabina.CabinaService;
+import edu.badpals.estudio.view.View;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -25,16 +26,16 @@ public class Controller {
     private Label lblTrabajadores;
 
     @FXML
-    private TableColumn<?, ?> rowID;
+    private TableColumn<Cabina, Integer> rowID;
 
     @FXML
-    private TableColumn<?, ?> rowPiercing;
+    private TableColumn<Cabina, Boolean> rowPiercing;
 
     @FXML
-    private TableColumn<?, ?> rowSuperficie;
+    private TableColumn<Cabina, Float> rowSuperficie;
 
     @FXML
-    private TableColumn<?, ?> rowUbicacion;
+    private TableColumn<Cabina, String> rowUbicacion;
 
     @FXML
     private TableView<Cabina> tblCabinas;
@@ -42,8 +43,6 @@ public class Controller {
     @FXML
     private TextField txtIdCabina;
 
-    @FXML
-    private TextField txtPiercingCabina;
 
     @FXML
     private TextField txtSuperficieCabina;
@@ -56,11 +55,41 @@ public class Controller {
 
     }
 
-    private final CabinaService cabinaService;
+    @FXML
+    private TextField txtPiercing;
+
+
+    @FXML
+    public void initialize() {
+
+
+        tblCabinas.setOnMouseClicked(event -> {
+
+            Cabina cabinaSeleccionada = tblCabinas.getSelectionModel().getSelectedItem();
+
+            if (cabinaSeleccionada != null) {
+
+                txtIdCabina.setText(String.valueOf(cabinaSeleccionada.getId()));
+                txtUbicacionCabina.setText(cabinaSeleccionada.getUbicacion());
+                txtSuperficieCabina.setText(String.valueOf(cabinaSeleccionada.getSuperficie()));
+                txtPiercing.setText(puedeHacerPiercingStr(cabinaSeleccionada.getPuedeHacerPiercing()));
+
+
+            }
+        });
+    }
+
+
 
     public Controller() {
         this.cabinaService = new CabinaService();
+        this.view = new View();
     }
+
+
+    private final CabinaService cabinaService;
+    private final View view;
+
 
     // TODO Load tabla tiene demasiadas responsabilidades, pero funciona, hay que dividir entre el "Asignar datos" y el "Rediseñar las celdas" en dos metodos diferentes.
 
@@ -69,6 +98,8 @@ public class Controller {
 
         List<Cabina> cabinasActuales = cabinaService.getAllCabinas();
         tblCabinas.getItems().addAll(cabinasActuales);
+
+
 
         rowID.setText("ID");
         rowUbicacion.setText("Ubicacion");
@@ -88,6 +119,93 @@ public class Controller {
         rowPiercing.setCellValueFactory(new PropertyValueFactory<>("puedeHacerPiercing"));
 
     }
+
+    private String puedeHacerPiercingStr(boolean puedeHacerPiercing) {
+        return puedeHacerPiercing ? "Sí" : "No";
+    }
+
+    private Boolean puedeHacerPiercingBool(String puedeHacerPiercing) {
+        return puedeHacerPiercing.equals("Sí");
+    }
+
+    @FXML
+    private void crearCabina (ActionEvent event) {
+
+        if (txtUbicacionCabina.getText().isEmpty() || txtSuperficieCabina.getText().isEmpty() || txtPiercing.getText().isEmpty()) {
+
+            view.lanzarMensajeError("Error", "Campos vacíos", "Por favor, rellene todos los campos");
+
+        } else if (cabinaService.getCabinaByUbicacion(txtUbicacionCabina.getText()) != null) {
+
+            view.lanzarMensajeError("Error", "Ubicación en uso", "La ubicación de la cabina ya exiete");
+
+        } else if (txtPiercing.getText().equals("Sí") || txtPiercing.getText().equals("No")) {
+
+
+            cabinaService.createCabina(txtUbicacionCabina.getText().toUpperCase(), Float.parseFloat(txtSuperficieCabina.getText()), puedeHacerPiercingBool(txtPiercing.getText()));
+
+            view.lanzarMensajeAviso("Cabina creada", "Cabina creada con éxito", "La cabina ha sido creada con éxito");
+
+            tblCabinas.getItems().clear();
+            loadTabla();
+
+        } else {
+
+            view.lanzarMensajeError("Error", "Valor incorrecto", "El campo 'Puede hacer piercing' solo puede ser 'Sí' o 'No'");
+
+        }
+    }
+
+
+    @FXML
+    private void updateCabina (ActionEvent event) {
+
+        if (txtUbicacionCabina.getText().isEmpty() || txtSuperficieCabina.getText().isEmpty() || txtPiercing.getText().isEmpty()) {
+
+            view.lanzarMensajeError("Error", "Campos vacíos", "Por favor, rellene todos los campos");
+
+        } else if (cabinaService.getCabinaByUbicacion(txtUbicacionCabina.getText()) != null) {
+
+            view.lanzarMensajeError("Error", "Ubicación en uso", "La ubicación de la cabina ya exiete");
+
+        } else if (txtPiercing.getText().equals("Sí") || txtPiercing.getText().equals("No")) {
+
+            Cabina cabina = cabinaService.getCabina(Integer.parseInt(txtIdCabina.getText()));
+
+            cabinaService.updateCabina(cabina, txtUbicacionCabina.getText().toUpperCase(), Float.parseFloat(txtSuperficieCabina.getText()), puedeHacerPiercingBool(txtPiercing.getText()));
+
+            view.lanzarMensajeAviso("Cabina actualizada", "Cabina actualizada con éxito", "La cabina ha sido actualizada con éxito");
+
+            tblCabinas.getItems().clear();
+            loadTabla();
+
+        } else {
+
+            view.lanzarMensajeError("Error", "Valor incorrecto", "El campo 'Puede hacer piercing' solo puede ser 'Sí' o 'No'");
+
+        }
+
+    }
+
+    @FXML
+    private void deleteCabina (ActionEvent event) {
+
+            if (txtIdCabina.getText().isEmpty()) {
+
+                view.lanzarMensajeError("Error", "No hay cabina seleccionada", "Por favor, seleccione en una cabina para eliminarla");
+
+            } else {
+
+                cabinaService.deleteCabina(Integer.parseInt(txtIdCabina.getText()));
+
+                view.lanzarMensajeAviso("Cabina eliminada", "Cabina eliminada con éxito", "La cabina ha sido eliminada con éxito");
+
+                tblCabinas.getItems().clear();
+                loadTabla();
+
+            }
+    }
+
 
 
 }
