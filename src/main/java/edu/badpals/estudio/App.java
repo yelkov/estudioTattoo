40ba1ebb6d1c05913d8059f6b0464d27de7ce050
Evan <evanchapela@gmail.com>
2025-01-05 04:12:10 +0100

    build(Controller): add basic items, load data in list

--- src/main/java/edu/badpals/estudio/App.java
+++ src/main/java/edu/badpals/estudio/App.java
@@ -1,14 +1,20 @@
 package edu.badpals.estudio;
 
+import edu.badpals.estudio.controller.Controller;
+import edu.badpals.estudio.model.cabina.CabinaService;
+import edu.badpals.estudio.model.utils.EntityManagerFactoryProvider;
 import javafx.application.Application;
+import javafx.application.ConditionalFeature;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
     public static void main(String[] args) {
         launch();
     }
+
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -17,5 +23,13 @@ public class App extends Application {
         stage.setTitle("Gestor de Tattoo Studio");
         stage.setScene(scene);
         stage.show();
+
+        EntityManagerFactoryProvider.initialize("test");
+
+
+        Controller controller = fxmlLoader.getController();
+        controller.loadTabla();
+
+
     }
 }
--- src/main/java/edu/badpals/estudio/controller/Controller.java
+++ src/main/java/edu/badpals/estudio/controller/Controller.java
@@ -1,10 +1,22 @@
 package edu.badpals.estudio.controller;
-
+import edu.badpals.estudio.model.cabina.Cabina;
+import edu.badpals.estudio.model.cabina.CabinaService;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import java.util.List;
 
 public class Controller {
+
+    @FXML
+    private Button btnActualizarCabina;
+
+    @FXML
+    private Button btnCrearCabina;
+
+    @FXML
+    private Button btnEliminarCabina;
 
     @FXML
     private Button btnTrabajadores;
@@ -13,9 +25,69 @@ public class Controller {
     private Label lblTrabajadores;
 
     @FXML
-    public void initialize(){}
+    private TableColumn<?, ?> rowID;
 
-    public void colocarTexto(){
-        lblTrabajadores.setText("TRABAJADORES");
+    @FXML
+    private TableColumn<?, ?> rowPiercing;
+
+    @FXML
+    private TableColumn<?, ?> rowSuperficie;
+
+    @FXML
+    private TableColumn<?, ?> rowUbicacion;
+
+    @FXML
+    private TableView<Cabina> tblCabinas;
+
+    @FXML
+    private TextField txtIdCabina;
+
+    @FXML
+    private TextField txtPiercingCabina;
+
+    @FXML
+    private TextField txtSuperficieCabina;
+
+    @FXML
+    private TextField txtUbicacionCabina;
+
+    @FXML
+    void colocarTexto(ActionEvent event) {
+
     }
+
+    private final CabinaService cabinaService;
+
+    public Controller() {
+        this.cabinaService = new CabinaService();
+    }
+
+    // TODO Load tabla tiene demasiadas responsabilidades, pero funciona, hay que dividir entre el "Asignar datos" y el "Rediseñar las celdas" en dos metodos diferentes.
+
+
+    public void loadTabla() {
+
+        List<Cabina> cabinasActuales = cabinaService.getAllCabinas();
+        tblCabinas.getItems().addAll(cabinasActuales);
+
+        rowID.setText("ID");
+        rowUbicacion.setText("Ubicacion");
+        rowSuperficie.setText("Superficie");
+        rowPiercing.setText("Puede hacer piercing");
+
+        tblCabinas.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        rowID.setMaxWidth(1f * Integer.MAX_VALUE * 10);
+        rowUbicacion.setMaxWidth(1f * Integer.MAX_VALUE * 40);
+        rowSuperficie.setMaxWidth(1f * Integer.MAX_VALUE * 30);
+        rowPiercing.setMaxWidth(1f * Integer.MAX_VALUE * 10);
+
+        rowID.setCellValueFactory(new PropertyValueFactory<>("id"));
+        rowUbicacion.setCellValueFactory(new PropertyValueFactory<>("ubicacion"));
+        rowSuperficie.setCellValueFactory(new PropertyValueFactory<>("superficie"));
+        rowPiercing.setCellValueFactory(new PropertyValueFactory<>("puedeHacerPiercing"));
+
+    }
+
+
 }
